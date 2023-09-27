f085f8311db9fa7a8098118a3d20e0cf7a93a043
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-09-27 22:22:50 +0200

    first Commit

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import "../styles/globals.css";
+import Head from "next/head";
+
+import Layout from "../Components/Layout";
+import Transition from "../Components/Transition";
+
+
+import { useRouter } from "next/router";
+
+import { AnimatePresence, motion } from "framer-motion";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/MV1.ico" />
+        <title>Maria Virk</title>
+      </Head>
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div key={router.route} className="h-full">
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
+  );
+}
+export default MyApp;
