53f851f84690fec8980f85a126e87d1a721e4b1e
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-10-12 14:49:21 +0200

    Overflow

--- pages/about/index.js
+++ pages/about/index.js
@@ -221,7 +221,7 @@ const About = () => {
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="flex flex-col w-full xl:max-w-[48%] h-[680px]"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px]  overflow-x-auto"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
