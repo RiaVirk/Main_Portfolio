cdbae5d9c48b9e64f6a514894f1bef3d6c831ec1
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-10-12 18:01:15 +0200

    Index File

--- pages/about/index.js
+++ pages/about/index.js
@@ -127,7 +127,7 @@ const About = () => {
   const [index, setIndex] = useState(0);
   console.log(index);
   return (
-    <div className="h-full bg-primary/30 py-48 text-center xl:text-left overflow-auto">
+    <div className="h-full bg-primary/30 py-48 text-center xl:text-left overflow-x-auto">
       <Circles />
 
       <motion.div
